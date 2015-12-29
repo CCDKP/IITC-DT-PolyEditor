cf64038929ccd56ba9e3166ba2e8318b215c3b8e
CCDKP <qcadmin@gmail.com>
2015-12-29 17:20:54 -0600

    Move everything inside polyedit object to fix scope issues

---
+++ polyedit.js
@@ -0,0 +1,115 @@
+// script.js
+
+//Globals
+angular.module('polyEdit', ['ui.router'])
+    
+
+//Routing
+.config(function($stateProvider, $urlRouterProvider) {
+
+	$stateProvider
+
+	// App function
+		.state('pe', {
+			url: '/polyedit',
+			templateUrl : 'polyedit.html',
+			controller: 'peController'
+		})
+
+		// JSON loader & validation
+		.state('pe.load', {
+			url: '/load',
+			templateUrl : 'polyedit-load.html',
+		})
+		
+		
+		// route for the about page
+		.state('pe.edit', {
+			url: '/edit',
+			templateUrl : 'polyedit-edit.html',
+			controller: function ($scope) {
+				$scope.isDataLoaded();
+				$scope.displayEdit();
+			}
+		})
+
+		// save out json
+		.state('pe.save', {
+			url: '/save',
+			templateUrl : 'polyedit-save.html',
+			controller: function ($scope) {
+				$scope.isDataLoaded();
+				$scope.displayEdit();
+			}
+			
+		})
+
+	$urlRouterProvider.otherwise('/polyedit/load');
+
+})
+	
+//Controller functions
+.controller('peController', function ($scope, $state) {
+	
+	$scope.input = "";
+	$scope.data = [];
+	$scope.polygons = 0;
+	$scope.polylines = 0;
+	$scope.circles = 0;
+	$scope.markers = 0;
+	
+	$scope.isDataLoaded = function () {
+		if ($scope.input.length < 1)
+			$state.go('^.load');
+	}
+		
+	$scope.loadData = function () {
+		try {
+			document.getElementById("alertBar").innerHTML = "";
+			if (document.getElementById("datain").value == '') throw "No data present";
+			$scope.input = JSON.parse(document.getElementById("datain").value);
+			if ($scope.input.length < 1) throw "Empty set detected;"
+		}
+		catch(err) {
+			 document.getElementById("alertBar").innerHTML = "<div class='alert alert-danger' role='alert'>" + err + " </div>";
+			 return;
+		}
+		$scope.data = $scope.input;
+		document.getElementById("editBtn").removeAttribute("disabled");
+		document.getElementById("saveBtn").removeAttribute("disabled");
+		$state.go('^.edit');
+	};
+	
+	$scope.displayEdit = function () {
+		$scope.polygons = 0;
+		$scope.polylines = 0;
+		$scope.circles = 0;
+		$scope.markers = 0;
+		for(var i=0;i<$scope.data.length;i++) {
+			switch($scope.data[i].type) {
+				case "polyline":
+					$scope.polylines++;
+					continue;
+				case "circle":
+					$scope.circles++;
+					continue;
+				case "marker":
+					$scope.markers++;
+					continue;
+				case "polygon":
+					$scope.polygons++;
+					continue;
+				default:
+					continue;
+			}
+		}
+
+	}
+	$scope.debugger = function () {
+		$scope.polygons++;
+	}
+	
+});
+	
+	
+	
